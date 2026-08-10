41c4a45d2b40ad71eb60fb24e90d301b824c2fa7
Anshul <anshul.soni@hbwsl.com>
2026-08-10 14:09:56 +0530

    fix permission callbacks for endpoints not using jwt

--- admin/class-gdpr-cookie-consent-admin.php
+++ admin/class-gdpr-cookie-consent-admin.php
@@ -10584,13 +10584,7 @@ class Gdpr_Cookie_Consent_Admin {
 			array(
 				'methods'  => 'POST',
 				'callback' => array($this, 'gdpr_send_data_to_dashboard_appwplp_server'), // Function to handle the request
-				'permission_callback' => function() use ($is_user_connected) {
-					// Check if user is connected and the API plan is valid
-					if ($is_user_connected) {
-						return true; // Allow access
-					}
-					return new WP_Error('rest_forbidden', 'Unauthorized access', array('status' => 401));
-				},
+				'permission_callback'	=> array( $this, 'permission_callback_for_react_app' ),
 			)
 		);
 		register_rest_route(
@@ -10644,13 +10638,7 @@ class Gdpr_Cookie_Consent_Admin {
 				array(
 					'methods'  => 'POST',
 					'callback' => array( $this, 'gdpr_get_wplp_payment_status' ),
-					'permission_callback' => function() use ( $is_user_connected ) {
-						// Check if user is connected and the API plan is valid.
-						if ( $is_user_connected ) {
-							return true; // Allow access.
-						}
-						return new WP_Error( 'rest_forbidden', 'Unauthorized access', array( 'status' => 401 ) );
-					},
+					'permission_callback' => array( $this, 'permission_callback_for_react_app' )
 				)
 			);
 		}
@@ -10662,14 +10650,7 @@ class Gdpr_Cookie_Consent_Admin {
 				array(
 					'methods'  => 'POST',
 					'callback' => array( $this, 'gdpr_set_subscription_payment_pending_cancel' ),
-					'permission_callback' => function() use ( $is_user_connected ) {
-						// Check if user is connected and the API plan is valid.
-						if ( $is_user_connected ) {
-							return true; // Allow access.
-						}
-						return new WP_Error( 'rest_forbidden', 'Unauthorized access', array( 'status' => 401 ) );
-					},
-				)
+					'permission_callback' => array( $this, 'permission_callback_for_react_app' )				)
 			);
 		}
 	}
