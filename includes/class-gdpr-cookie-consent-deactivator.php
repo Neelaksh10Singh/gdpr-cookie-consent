46951ef7ca5fb6763f4c5f654b5e00e0bbd96d33
Anshul <anshul.soni@hbwsl.com>
2026-08-31 09:36:34 +0530

    bugfixes 4.4.2

--- includes/class-gdpr-cookie-consent-deactivator.php
+++ includes/class-gdpr-cookie-consent-deactivator.php
@@ -61,10 +61,15 @@ class Gdpr_Cookie_Consent_Deactivator {
 		$the_options['is_eu_on'] = 'false';
 		$the_options['is_ccpa_on'] = 'false';
 		update_option( GDPR_COOKIE_CONSENT_SETTINGS_FIELD, $the_options );
-
-		$timestamp = wp_next_scheduled( 'appwplp_secret_key_retry_event' );
-		if ( $timestamp ) {
-			wp_clear_scheduled_hook('appwplp_secret_key_retry_event' );
+		$wplp_plugin_name = 'wplegalpages/wplegalpages.php';
+		if ( ! function_exists( 'is_plugin_active' ) ) {
+			require_once ABSPATH . 'wp-admin/includes/plugin.php';
+		}
+		if ( ! is_plugin_active( $wplp_plugin_name ) ) {
+			$timestamp = wp_next_scheduled( 'appwplp_secret_key_retry_event' );
+			if ( $timestamp ) {
+				wp_clear_scheduled_hook('appwplp_secret_key_retry_event' );
+			}
 		}
 
 		update_option( 'gdpr_no_of_page_scan', 0 );
